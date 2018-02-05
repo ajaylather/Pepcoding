9d73454897a06b8677c407676130641723cac3ed
ajaylather <ajaylather2050@gmail.com>
2018-02-05 21:49:31 +0530

    Changed MCM

--- Pepcoding/src/feb_4_Matrix_Multiplication/mcm.java
+++ Pepcoding/src/feb_4_Matrix_Multiplication/mcm.java
@@ -7,9 +7,9 @@ public class mcm {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] dims= {10,20,30,40,50,60};
-		System.out.println(mcm(dims, 0, dims.length-1, new int[dims.length][dims.length]));
+		System.out.println(mat_multiplication(dims, 0, dims.length-1, new int[dims.length][dims.length]));
 	}
-	public static int mcm(int[] dims,int si,int ei,int[][] strg) {
+	public static int mat_multiplication(int[] dims,int si,int ei,int[][] strg) {
 		if(si==ei-1) {
 			return 0;
 		}
@@ -18,8 +18,8 @@ public class mcm {
 		}
 		int mincost=Integer.MAX_VALUE;
 		for(int i=si+1;i<ei;i++) {
-			int cost1=mcm(dims,si,i,strg);
-			int cost2=mcm(dims,i,ei,strg);
+			int cost1=mat_multiplication(dims,si,i,strg);
+			int cost2=mat_multiplication(dims,i,ei,strg);
 			int cost3=dims[si]*dims[i]*dims[ei];
 			int totalcost=cost1+cost2+cost3;
 			
